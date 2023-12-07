76b9c947ecc4a3915d175f1667fb869c533bd9a9
vanshgautam03 <vanshgautam620@gmail.com>
2023-12-07 12:51:15 -0500

    done

--- Data/DbIntializer.cs
+++ Data/DbIntializer.cs
@@ -5,29 +5,29 @@ namespace project.Models
     public class DbInitializer
     {
         public static async Task Initialize(
-        UserManager<IdentityUser> userManager,
-        RoleManager<IdentityRole> roleManager)
-        {
-            string[] roleNames = { "Admin", "Customer", "DepartmentAdmin" };
-            foreach (var roleName in roleNames)
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
             {
-                var roleExist = await roleManager.RoleExistsAsync(roleName);
-                if (!roleExist)
-                    await roleManager.CreateAsync(new IdentityRole(roleName));
-            }
+                string[] roleNames = { "Admin", "Customer", "DepartmentAdmin" };
+                foreach (var roleName in roleNames)
+                {
+                    var roleExist = await roleManager.RoleExistsAsync(roleName);
+                    if (!roleExist)
+                        await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
 
-            // Create an Admin user
-            var user = new IdentityUser
-            {
-                UserName = "admin@admin.com",
-                Email = "admin@admin.com",
-            };
+                // Create an Admin user
+                var user = new IdentityUser
+                {
+                    UserName = "admin@admin.com",
+                    Email = "admin@admin.com",
+                };
 
-            string userPWD = "Password@123";
-            var createUser = await userManager.CreateAsync(user, userPWD);
+                string userPWD = "Password@123";
+                var createUser = await userManager.CreateAsync(user, userPWD);
 
-            if (createUser.Succeeded)
-                await userManager.AddToRoleAsync(user, "Admin");
+                if (createUser.Succeeded)
+                    await userManager.AddToRoleAsync(user, "Admin");
         }
     }
 }
